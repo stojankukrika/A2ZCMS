c1083a8e5d8d51397ab0184c65d5fa4cf36b983d
stojan.kukrika <mrakodol@gmail.com>
2013-10-31 06:31:20 +0100

    Add meesage database table

---
+++ app/database/migrations/2013_10_31_062801_create_meesages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMeesagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('meesages', function(Blueprint $table) {
+			$table->increments('id');
+			$table -> integer('user_id_from') -> unsigned();
+			$table -> foreign('user_id_from') -> references('id') -> on('users');
+			$table -> integer('user_id_to') -> unsigned();
+			$table -> foreign('user_id_to') -> references('id') -> on('users');
+			$table -> text('content');
+			$table -> boolean('read');
+			$table -> timestamps();
+			$table -> softDeletes();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('meesages');
+	}
+
+}
