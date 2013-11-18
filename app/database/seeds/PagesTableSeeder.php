dcbeb9cbd68de9025c934131613272f9ec67360a
stojan.kukrika <mrakodol@gmail.com>
2013-11-18 22:28:52 +0100

    fix link to issue in page seeder

--- app/database/seeds/PagesTableSeeder.php
+++ app/database/seeds/PagesTableSeeder.php
@@ -6,7 +6,7 @@ class PagesTableSeeder extends Seeder {
 	{
 		// Uncomment the below to wipe the table clean before populating
 		// DB::table('pages')->truncate();
-		$ontent = '<div><h1>A2Z CMS 1.0</h1><p>Welcome to your very own A2Z CMS 1.1 installation.</p></div><div><p>Login into your profile and change this page and enjoy in A2ZCMS.</p><p>If you have any questions feel free to check the <a href=\"https://github.com/mrakodol/A2ZCMS/issues\">Issues</a> at any time or create a new issue.</p><p>Enjoy A2Z CMS and welcome a board.</p><p>Kind Regards</p><p>Stojan Kukrika - A2Z CMS</p></div>';
+		$ontent = '<div><h1>A2Z CMS 1.0</h1><p>Welcome to your very own A2Z CMS 1.1 installation.</p></div><div><p>Login into your profile and change this page and enjoy in A2ZCMS.</p><p>If you have any questions feel free to check the <a href="https://github.com/mrakodol/A2ZCMS/issues">Issues</a> at any time or create a new issue.</p><p>Enjoy A2Z CMS and welcome a board.</p><p>Kind Regards</p><p>Stojan Kukrika - A2Z CMS</p></div>';
 		$pages = array( 
 					array('name' => 'Home', 
 						'slug' => 'home',
