cf09b1f23cb99c98cd030697fe26b1600f6ebc94
stojan.kukrika <mrakodol@gmail.com>
2013-12-18 06:27:51 +0100

    fix in routes and composer.json

--- app/routes.php
+++ app/routes.php
@@ -34,24 +34,11 @@ Route::model('customformfields', 'CustomFormField');
  *  Route constraint patterns
  *  ------------------------------------------
  */
-Route::pattern('blog', '[0-9]+');
-Route::pattern('blogcomment', '[0-9]+');
-Route::pattern('blogcategory', '[0-9]+');
-Route::pattern('gallery', '[0-9]+');
-Route::pattern('galleryimagecomment', '[0-9]+');
-Route::pattern('galleryimage', '[0-9]+');
-Route::pattern('nav', '[0-9]+');
-Route::pattern('group', '[0-9]+');
-Route::pattern('role', '[0-9]+');
-Route::pattern('user', '[0-9]+');
-Route::pattern('todo', '[0-9]+');
-Route::pattern('cform', '[0-9]+');
-Route::pattern('messageid', '[0-9]+');
+Route::pattern('id', '[0-9]+');
+Route::pattern('id2', '[0-9]+');
+Route::pattern('id3', '[0-9]+');
 Route::pattern('token', '[0-9a-z-]+');
 
-Route::pattern('postSlug', '[0-9a-z-]+');
-Route::pattern('postSlug1', '[0-9]+');
-Route::pattern('postSlug2', '[0-9]+');
 
 /** ------------------------------------------
  *  Admin Routes
@@ -61,109 +48,109 @@ Route::group(array('prefix' => 'admin', 'before' => 'auth|detectLang'), function
 {
 
     # Blog Comment Management
-    Route::get('blogcomments/{blog}/commentsforblog', 'AdminBlogCommentController@getCommentsForBlog');
-    Route::get('blogcomments/{blogcomment}/edit', 'AdminBlogCommentController@getEdit');
-    Route::post('blogcomments/{blogcomment}/edit', 'AdminBlogCommentController@postEdit');
-    Route::get('blogcomments/{blogcomment}/delete', 'AdminBlogCommentController@getDelete');
-    Route::post('blogcomments/{blogcomment}/delete', 'AdminBlogCommentController@getDelete');
+    Route::get('blogcomments/{id}/commentsforblog', 'AdminBlogCommentController@getCommentsForBlog');
+    Route::get('blogcomments/{id}/edit', 'AdminBlogCommentController@getEdit');
+    Route::post('blogcomments/{id}/edit', 'AdminBlogCommentController@postEdit');
+    Route::get('blogcomments/{id}/delete', 'AdminBlogCommentController@getDelete');
+    Route::post('blogcomments/{id}/delete', 'AdminBlogCommentController@getDelete');
     Route::controller('blogcomments', 'AdminBlogCommentController');
 	
 	 # Blog Category Management
-    Route::get('blogcategorys/{blogcategory}/edit', 'AdminBlogCategoryController@getEdit');
-    Route::post('blogcategorys/{blogcategory}/edit', 'AdminBlogCategoryController@postEdit');
-    Route::get('blogcategorys/{blogcategory}/delete', 'AdminBlogCategoryController@getDelete');
-    Route::post('blogcategorys/{blogcategory}/delete', 'AdminBlogCategoryController@getDelete');
+    Route::get('blogcategorys/{id}/edit', 'AdminBlogCategoryController@getEdit');
+    Route::post('blogcategorys/{id}/edit', 'AdminBlogCategoryController@postEdit');
+    Route::get('blogcategorys/{id}/delete', 'AdminBlogCategoryController@getDelete');
+    Route::post('blogcategorys/{id}/delete', 'AdminBlogCategoryController@getDelete');
     Route::controller('blogcategorys', 'AdminBlogCategoryController');
 
     # Blog Management
-    Route::get('blogs/{blog}/edit', 'AdminBlogController@getEdit');
-    Route::post('blogs/{blog}/edit', 'AdminBlogController@postEdit');
-    Route::get('blogs/{blog}/delete', 'AdminBlogController@getDelete');
-    Route::post('blogs/{blog}/delete', 'AdminBlogController@getDelete');
+    Route::get('blogs/{id}/edit', 'AdminBlogController@getEdit');
+    Route::post('blogs/{id}/edit', 'AdminBlogController@postEdit');
+    Route::get('blogs/{id}/delete', 'AdminBlogController@getDelete');
+    Route::post('blogs/{id}/delete', 'AdminBlogController@getDelete');
     Route::controller('blogs', 'AdminBlogController');
 	
 	 # Gallery Comment Management
-    Route::get('galleryimagecomments/{gallery}/commentsforgallery', 'AdminGalleryImageCommentController@getCommentsforgallery');
-    Route::get('galleryimagecomments/{galleryimagecomment}/edit', 'AdminGalleryImageCommentController@getEdit');
-    Route::post('galleryimagecomments/{galleryimagecomment}/edit', 'AdminGalleryImageCommentController@postEdit');
-    Route::get('galleryimagecomments/{galleryimagecomment}/delete', 'AdminGalleryImageCommentController@getDelete');
-    Route::post('galleryimagecomments/{galleryimagecomment}/delete', 'AdminGalleryImageCommentController@getDelete');
+    Route::get('galleryimagecomments/{id}/commentsforgallery', 'AdminGalleryImageCommentController@getCommentsforgallery');
+    Route::get('galleryimagecomments/{id}/edit', 'AdminGalleryImageCommentController@getEdit');
+    Route::post('galleryimagecomments/{id}/edit', 'AdminGalleryImageCommentController@postEdit');
+    Route::get('galleryimagecomments/{id}/delete', 'AdminGalleryImageCommentController@getDelete');
+    Route::post('galleryimagecomments/{id}/delete', 'AdminGalleryImageCommentController@getDelete');
     Route::controller('galleryimagecomments', 'AdminGalleryImageCommentController');
 	
 	 # Gallery Images Management
-    Route::get('galleryimages/{galleryimage}/delete', 'AdminGalleryImageController@postDelete');
+    Route::get('galleryimages/{id}/delete', 'AdminGalleryImageController@postDelete');
     Route::controller('galleryimages', 'AdminGalleryImageController');
 	
     # Galleries Management
-    Route::get('galleries/{gallerycategory}/imagesforgallery', 'AdminGalleryController@getImagesForGallery');
-    Route::get('galleries/{gallery}/edit', 'AdminGalleryController@getEdit');
-    Route::post('galleries/{gallery}/edit', 'AdminGalleryController@postEdit');
-    Route::get('galleries/{gallery}/delete', 'AdminGalleryController@getDelete');
-    Route::post('galleries/{gallery}/delete', 'AdminGalleryController@getDelete');
-	Route::get('galleries/{gallery}/upload', 'AdminGalleryController@getUpload');
-    Route::post('galleries/{gallery}/upload', 'AdminGalleryController@postUpload');		
+    Route::get('galleries/{id}/imagesforgallery', 'AdminGalleryController@getImagesForGallery');
+    Route::get('galleries/{id}/edit', 'AdminGalleryController@getEdit');
+    Route::post('galleries/{id}/edit', 'AdminGalleryController@postEdit');
+    Route::get('galleries/{id}/delete', 'AdminGalleryController@getDelete');
+    Route::post('galleries/{id}/delete', 'AdminGalleryController@getDelete');
+	Route::get('galleries/{id}/upload', 'AdminGalleryController@getUpload');
+    Route::post('galleries/{id}/upload', 'AdminGalleryController@postUpload');		
     Route::controller('galleries', 'AdminGalleryController');
 	
 	# Navigation Management
-	Route::get('pages/{nav}/visible', 'AdminPageController@getVisible');
-    Route::get('pages/{nav}/edit', 'AdminPageController@getEdit');
-    Route::post('pages/{nav}/edit', 'AdminPageController@postEdit');
-    Route::get('pages/{nav}/delete', 'AdminPageController@getDelete');
-    Route::post('pages/{nav}/delete', 'AdminPageController@getDelete');
+	Route::get('pages/{id}/visible', 'AdminPageController@getVisible');
+    Route::get('pages/{id}/edit', 'AdminPageController@getEdit');
+    Route::post('pages/{id}/edit', 'AdminPageController@postEdit');
+    Route::get('pages/{id}/delete', 'AdminPageController@getDelete');
+    Route::post('pages/{id}/delete', 'AdminPageController@getDelete');
     Route::controller('pages', 'AdminPageController');
     
     # Navigation Group Management
-    Route::get('navigationgroups/{group}/edit', 'AdminNavigationGroupController@getEdit');
-    Route::post('navigationgroups/{group}/edit', 'AdminNavigationGroupController@postEdit');
-    Route::get('navigationgroups/{group}/delete', 'AdminNavigationGroupController@getDelete');
-    Route::post('navigationgroups/{group}/delete', 'AdminNavigationGroupController@getDelete');
+    Route::get('navigationgroups/{id}/edit', 'AdminNavigationGroupController@getEdit');
+    Route::post('navigationgroups/{id}/edit', 'AdminNavigationGroupController@postEdit');
+    Route::get('navigationgroups/{id}/delete', 'AdminNavigationGroupController@getDelete');
+    Route::post('navigationgroups/{id}/delete', 'AdminNavigationGroupController@getDelete');
     Route::controller('navigationgroups', 'AdminNavigationGroupController');
 
     # Navigation Management
-    Route::get('navigation/{nav}/edit', 'AdminNavigationController@getEdit');
-    Route::post('navigation/{nav}/edit', 'AdminNavigationController@postEdit');
-    Route::get('navigation/{nav}/delete', 'AdminNavigationController@getDelete');
-    Route::post('navigation/{nav}/delete', 'AdminNavigationController@getDelete');
+    Route::get('navigation/{id}/edit', 'AdminNavigationController@getEdit');
+    Route::post('navigation/{id}/edit', 'AdminNavigationController@postEdit');
+    Route::get('navigation/{id}/delete', 'AdminNavigationController@getDelete');
+    Route::post('navigation/{id}/delete', 'AdminNavigationController@getDelete');
     Route::controller('navigation', 'AdminNavigationController');
 
     # User Management    
-    Route::get('users/{role}/usersforrole', 'AdminUserController@getUsersForRole');
-	Route::get('users/{user}/usershistory', 'AdminUserController@getHistory');
-    Route::get('users/{user}/edit', 'AdminUserController@getEdit');
-    Route::post('users/{user}/edit', 'AdminUserController@postEdit');
-    Route::get('users/{user}/delete', 'AdminUserController@getDelete');
-    Route::post('users/{user}/delete', 'AdminUserController@getDelete');
+    Route::get('users/{id}/usersforrole', 'AdminUserController@getUsersForRole');
+	Route::get('users/{id}/usershistory', 'AdminUserController@getHistory');
+    Route::get('users/{id}/edit', 'AdminUserController@getEdit');
+    Route::post('users/{id}/edit', 'AdminUserController@postEdit');
+    Route::get('users/{id}/delete', 'AdminUserController@getDelete');
+    Route::post('users/{id}/delete', 'AdminUserController@getDelete');
 	#Profile
 	Route::get('users/profile', 'AdminUserController@getProfileEdit');
     Route::post('users/profile', 'AdminUserController@postProfileEdit');	
     Route::controller('users', 'AdminUserController');
 
     # User Role Management
-    Route::get('roles/{role}/edit', 'AdminRoleController@getEdit');
-    Route::post('roles/{role}/edit', 'AdminRoleController@postEdit');
-    Route::get('roles/{role}/delete', 'AdminRoleController@getDelete');
-    Route::post('roles/{role}/delete', 'AdminRoleController@getDelete');
+    Route::get('roles/{id}/edit', 'AdminRoleController@getEdit');
+    Route::post('roles/{id}/edit', 'AdminRoleController@postEdit');
+    Route::get('roles/{id}/delete', 'AdminRoleController@getDelete');
+    Route::post('roles/{id}/delete', 'AdminRoleController@getDelete');
     Route::controller('roles', 'AdminRoleController');
 
 	# To-do list
-	Route::get('todolists/{todo}/change', 'AdminTodolistController@getChange');
-    Route::get('todolists/{todo}/edit', 'AdminTodolistController@getEdit');
-    Route::post('todolists/{todo}/edit', 'AdminTodolistController@postEdit');
-    Route::get('todolists/{todo}/delete', 'AdminTodolistController@getDelete');
-    Route::post('todolists/{todo}/delete', 'AdminTodolistController@getDelete');
+	Route::get('todolists/{id}/change', 'AdminTodolistController@getChange');
+    Route::get('todolists/{id}/edit', 'AdminTodolistController@getEdit');
+    Route::post('todolists/{id}/edit', 'AdminTodolistController@postEdit');
+    Route::get('todolists/{id}/delete', 'AdminTodolistController@getDelete');
+    Route::post('todolists/{id}/delete', 'AdminTodolistController@getDelete');
     Route::controller('todolists', 'AdminTodolistController');
 
  	# Custom form Management   
-    Route::get('customform/{cform}/edit', 'AdminCustomFormController@getEdit');
-    Route::post('customform/{cform}/edit', 'AdminCustomFormController@postEdit');
-    Route::get('customform/{cform}/delete', 'AdminCustomFormController@getDelete');
-    Route::post('customform/{cform}/delete', 'AdminCustomFormController@getDelete');
+    Route::get('customform/{id}/edit', 'AdminCustomFormController@getEdit');
+    Route::post('customform/{id}/edit', 'AdminCustomFormController@postEdit');
+    Route::get('customform/{id}/delete', 'AdminCustomFormController@getDelete');
+    Route::post('customform/{id}/delete', 'AdminCustomFormController@getDelete');
     Route::controller('customform', 'AdminCustomFormController');
 
 	# Settings
     Route::get('settings', 'AdminSettingsController@getIndex');
     Route::post('settings', 'AdminSettingsController@postIndex');
-		
+			
     # Admin Dashboard
     Route::controller('/', 'AdminDashboardController');
 	
@@ -179,10 +166,10 @@ Route::group(array('prefix' => 'admin', 'before' => 'auth|detectLang'), function
  Route::group(array('before' => 'auth'), function()
 {
     //:: User Account Routes ::
-	Route::post('user/{user}/edit', 'UserController@postEdit');
+	Route::post('user/{id}/edit', 'UserController@postEdit');
 	//User messages
 	Route::get('user/messages', 'UserMessagesController@getIndex');
-	Route::get('user/messages/{messageid}/read', 'UserMessagesController@getRead');
+	Route::get('user/messages/{id}/read', 'UserMessagesController@getRead');
 	Route::post('user/messages/sendmessage', 'UserMessagesController@postSendmessage');
 	
 });
@@ -211,19 +198,19 @@ Route::controller('install', 'InstallController');
 //:: Application Routes ::
 
 # Posts - Second to last set, match slug
-Route::get('blog/{postSlug}', 'BlogController@getView');
-Route::post('blog/{postSlug}', 'BlogController@postView');
+Route::get('blog/{id}', 'BlogController@getView');
+Route::post('blog/{id}', 'BlogController@postView');
 
-Route::get('gallery/{postSlug1}', 'GalleryController@getView');
-Route::get('galleryimage/{postSlug1}/{postSlug2}', 'GalleryController@getGalleryImage');
-Route::post('galleryimage/{postSlug1}/{postSlug2}', 'GalleryController@postGalleryImage');
+Route::get('gallery/{id}', 'GalleryController@getView');
+Route::get('galleryimage/{id}/{id2}', 'GalleryController@getGalleryImage');
+Route::post('galleryimage/{id}/{id2}', 'GalleryController@postGalleryImage');
 
-Route::get('page/{postSlug1}', 'WebsiteController@getView');
-Route::post('page/{postSlug1}', 'WebsiteController@postView');
+Route::get('page/{id}', 'WebsiteController@getView');
+Route::post('page/{id}', 'WebsiteController@postView');
 Route::get('contentvote', 'WebsiteController@contentvote');
 
-Route::get('customform/{postSlug1}', 'CustomFormController@postView');
-Route::post('customform/{postSlug1}', 'CustomFormController@postView');
+Route::get('customform/{id}', 'CustomFormController@postView');
+Route::post('customform/{id}', 'CustomFormController@postView');
 
 # Offline Static Page
 Route::get('offline', function()
